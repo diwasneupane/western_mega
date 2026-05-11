f57433861eed5079c999df0a56b4a3ee749c660a
unknown <diwas.n@medex.co.th>
2026-05-11 11:31:58 +0545

    Updated vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "westernmegabackend.e-aribt.com" },
       { protocol: "https", hostname: "westernmegacollege.edu.np" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
 };
